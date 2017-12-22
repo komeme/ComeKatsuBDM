d3d79a5f252063ab5c2159402dc305556b7ce6c0
Comeme <developer.k.mshr0316@gmail.com>
2017-12-22 11:27:45 +0900

    音源を再生するplay_soundを追加

---
+++ examples/play_sound.py
@@ -0,0 +1,9 @@
+#-*- cording: utf-8 -*-
+
+import subprocess
+
+
+def play_sound(filename):
+    # 音源を再生する関数
+    #subprocess.call("play " + filename, shell=True)
+    subprocess.call("aplay " + filename, shell=True)
